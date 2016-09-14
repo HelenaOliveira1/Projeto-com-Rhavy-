0c30d4998c3272cabe17575505c9a5e99da69129
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-14 17:38:16 -0300

    Exercícios com Listas
    Listas são uma coleção ordenada de valores, identificadas por índices
    que começam a partir do numeral zero.

---
+++ Exercício Listas/Questão 1.py
@@ -0,0 +1,13 @@
+print("Questão 1")
+print("")
+
+lista_v = []
+
+for i in range(1,6):
+    vetor = int(input("Digite um número: "))
+
+    lista_v.append(vetor)
+
+print("")
+print(lista_v)
+             
---
+++ Exercício Listas/Questão 2.py
@@ -0,0 +1,13 @@
+print("Questão 2")
+print("")
+
+lista = []
+
+for i in range(1,11):
+    vetor = float(input("Digite um número: "))
+
+    lista.append(vetor)
+
+print("")
+print(lista[::-1])
+             
